07e59c182df083ce79dd58e2bf41f94d44315543
Rafael Pallisser de Oliveira <rafaelpallisser@gmail.com>
2019-11-01 00:56:20 -0300

    Definindo PropTypes Correcao

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
 
-// import { Container } from './styles';
-
 export default class Repository extends Component {
   static propTypes = {
     match: PropTypes.shape({
       params: PropTypes.shape({
         repository: PropTypes.string,
       }),
-    }).isRequired(),
+    }).isRequired,
   };
 
   state = {
